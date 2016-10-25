ce6df599e027e86ae64ed7bfcf9baea1ec67a368
Alexander Bazo <alexanderbazo@googlemail.com>
2016-10-25 16:29:39 +0200

    Adding solution

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,20 +1,40 @@
 function init() {
-    changeName("your name");
+    changeName("Alexander Bazo");
     removeClass();
     initClickEvents();
     addBox();
 }
 
+
 function changeName(name) {
+    var el = document.querySelector(".name");
+    el.innerHTML = name;
 }
 
 function removeClass() {
+    var el = document.querySelector(".name");
+    el.classList.remove("placeholder");
 }
 
 function initClickEvents() {
+    var boxes = document.querySelectorAll(".boxes");
+    for (var i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener("click", onBoxClicked);
+    }
+}
+
+function onBoxClicked(event) {
+    var el = event.target;
+    el.classList.toggle("black");
+    el.classList.toggle("white");
 }
 
 function addBox() {
+	var parentEl = document.querySelector(".boxes");
+	var el = document.createElement("span");
+	el.classList.add("box", "red");
+	parentEl.append(el);
 }
 
 init();
+
